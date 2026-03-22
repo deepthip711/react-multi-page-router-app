2305b812ee79b1779e23d404533bc3586fd5a39d
Deepthi Pradeep <deepthip711@gmail.com>
2026-03-22 23:23:20 +0530

    Add files via upload

---
+++ src/pages/About.js
@@ -0,0 +1,15 @@
+function About() {
+  return (
+    <div className="container">
+      <div className="card">
+        <h2>About Me 👩‍💻</h2>
+        <p>
+          I am Deepthi, a BE student passionate about web development and building
+          projects using React.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
