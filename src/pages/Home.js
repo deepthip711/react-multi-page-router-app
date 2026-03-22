d570b096c6f66c07a06f1f41989ac23c4ec029d0
Deepthi Pradeep <deepthip711@gmail.com>
2026-03-22 23:12:03 +0530

    Add files via upload

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+function Home() {
+  return (
+    <div className="container">
+      <div className="card">
+        <h2>Welcome to Deepthi’s App 🚀</h2>
+        <p>This is a multi-page React app using React Router.</p>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/pages/NotFound.js
@@ -0,0 +1,12 @@
+function NotFound() {
+  return (
+    <div className="container">
+      <div className="card">
+        <h2>404 ❌</h2>
+        <p>Oops! Page not found.</p>
+      </div>
+    </div>
+  );
+}
+
+export default NotFound;
